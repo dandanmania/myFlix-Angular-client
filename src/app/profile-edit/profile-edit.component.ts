1a27b5a8e3c71f1ff5edc97cdc2f0b93ce6a526e
dandanmania <danielkimus@gmail.com>
2022-08-31 20:58:33 -0700

    Add profile (and profile editing) components

---
+++ src/app/profile-edit/profile-edit.component.ts
@@ -0,0 +1,53 @@
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DeleteUserWarningComponent } from '../delete-user-warning/delete-user-warning.component';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styleUrls: ['./profile-edit.component.scss']
+})
+export class ProfileEditComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: ''};
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      Username: string,
+    },
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<ProfileEditComponent>,
+    public fetchApiData: UserRegistrationService,
+    public snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  editUser(): void {
+    console.log(this.userData);
+    this.fetchApiData.editUser(this.userData).subscribe((result) => {
+      console.log(result)
+      this.dialogRef.close();
+      this.snackBar.open('Update successful!', 'OK', {
+        duration: 2000
+      });
+      setTimeout(function() {
+        window.location.reload();
+      }, 4500)
+    })
+  }
+
+  openDeleteUserDialog(): void {
+    this.dialogRef.close()
+    this.dialog.open(DeleteUserWarningComponent, {
+      width: '500px'
+    })
+  }
+
+}
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { ProfileEditComponent } from '../profile-edit/profile-edit.component';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent extends MovieCardComponent implements OnInit {
+
+  constructor(fetchApiData: UserRegistrationService, dialog: MatDialog) {
+    super(fetchApiData, dialog);
+  }
+
+  ngOnInIt(): void {}
+
+  openProfileEdit(username: string): void {
+    this.dialog.open(ProfileEditComponent, {
+      data: {
+        Username: username
+      },
+      width: '500px'
+    })
+  }
+
+}
