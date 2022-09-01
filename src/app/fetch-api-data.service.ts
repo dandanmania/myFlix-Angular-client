e53098fcb73502c6d267cca36e88c6ea23049ba0
dandanmania <danielkimus@gmail.com>
2022-08-31 20:57:32 -0700

    Update fetch-api-data service with getAllDirectors+Genres and fix false error responses

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -41,6 +41,28 @@ export class UserRegistrationService {
     );
   }
 
+  // Api call for Getting All Directors
+  getAllDirectors(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'directors', {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    )
+  }
+
+  // Api call for Getting All Directors
+  getAllGenres(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'genre', {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    )
+  }
+
   // Api call for Getting One Movie (by ID)
   getOneMovie(movieID: any): Observable<any> {
     const token = localStorage.getItem('token');
@@ -89,8 +111,9 @@ export class UserRegistrationService {
   addFavorite(movieID: any): Observable<any> {
     const token = localStorage.getItem('token');
     const username = localStorage.getItem('user');
-    return this.http.post(apiUrl + `${username}/movies/${movieID}`, {}, {
-      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+    return this.http.post(apiUrl + `users/${username}/movies/${movieID}`, {}, {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token }),
+      responseType: 'text'
     }).pipe(
       map(this.extractResponseData),
       catchError(this.handleError)
@@ -113,7 +136,8 @@ export class UserRegistrationService {
   deleteUser(username: any): Observable<any> {
     const token = localStorage.getItem('token');
     return this.http.delete(apiUrl + `users/${username}`, {
-      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token }),
+      responseType: 'text'
     }).pipe(
       map(this.extractResponseData),
       catchError(this.handleError)
@@ -123,8 +147,9 @@ export class UserRegistrationService {
   // Api call for Deleting Favorite Movie
   deleteFavorite(username: any, movieID: any): Observable<any> {
     const token = localStorage.getItem('token');
-    return this.http.delete(apiUrl + `${username}/movies/${movieID}`, {
-      headers: new HttpHeaders({ Authorization: 'Bearer ' + token })
+    return this.http.delete(apiUrl + `users/${username}/movies/${movieID}`, {
+      headers: new HttpHeaders({ Authorization: 'Bearer ' + token }),
+      responseType: 'text'
     }).pipe(
       map(this.extractResponseData),
       catchError(this.handleError)
@@ -137,7 +162,7 @@ export class UserRegistrationService {
     } else {
       console.error(
         `Error Status code ${error.status}, ` +
-        `Error body is: ${error.error}`
+        `Error body is: ${error.error.error}`
       );
     }
     return throwError(
