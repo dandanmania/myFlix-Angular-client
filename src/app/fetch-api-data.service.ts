34cd47959420226307c1c139d9300c483c2341f5
dandanmania <danielkimus@gmail.com>
2023-01-13 17:54:26 -0500

    Change to API link to cylic

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://dandan-myflix.herokuapp.com/'
+const apiUrl = 'https://dandanmania-movieapi.cyclic.app/'
 @Injectable({
   providedIn: 'root'
 })
